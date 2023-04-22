f9b1a00b8250b74529b15148bd9facf46ef376de
bwkw <na.shota.1139@gmail.com>
2023-04-21 21:52:01 -0700

    :fire: Remove AWS key info from variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,6 @@
 # ---------------------------
 # 変数設定
 # ---------------------------
-variable "access_key" {}
-variable "secret_key" {}
 variable "my_ip" {}
 variable "availability_zone_a" {
   default = "ap-northeast-1a"
