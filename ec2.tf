2c89fee9390d019469114457d5e4fc225908d1a3
bwkw <na.shota.1139@gmail.com>
2023-05-06 21:40:27 -0700

    :sparkles: Change EC2 resource

--- ec2.tf
+++ ec2.tf
@@ -3,21 +3,38 @@
 # ---------------------------
 
 # Amazon Linux 2 の最新版AMIを取得
-data "aws_ssm_parameter" "amzn2_latest_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+data "aws_ami" "amazon_linux_2_arm64" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-arm64-gp2"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["amazon"]
 }
 
 # EC2作成
 resource "aws_instance" "ec2" {
-  ami                         = data.aws_ssm_parameter.amzn2_latest_ami.value
-  instance_type               = "t2.micro"
+  ami                         = data.aws_ami.amazon_linux_2_arm64.id
+  instance_type               = var.ec2_instance_type
   availability_zone           = var.availability_zone_a
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   subnet_id                   = aws_subnet.pub_sub_1a.id
   associate_public_ip_address = "true"
   iam_instance_profile        = aws_iam_instance_profile.systems_manager.name
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = 20
+  }
   tags = {
-    Name = "terraform-ec2"
+    Name = "${var.environment}-${var.app_name}-ec2"
   }
 }
 
